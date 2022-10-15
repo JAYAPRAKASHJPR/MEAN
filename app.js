4c5837b1f431fc04a9ec068084dfd949fe0739dc
JAYAPRAKASHJPR <jayaprakashssakthivel@gmail.com>
2022-10-15 10:18:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const express  =  require('express');
+const app      =  express();
+const router   = require('./app/Routes/routes');
+
+app.use(express.static(__dirname + '/dist/'));
+
+app.use("/", router)
+
+
+
+
+app.listen(3000, () => {
+    console.log("App running on port number 3000")
+})
